91653ace152df8c6327a5c82b4cd34656501da63
trisjones <trisjones04@gmail.com>
2018-07-25 14:05:26 -0400

    Tested and done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,40 @@
+
 class Artist
 
-  attr_reader :name
+  attr_reader :artist_name, :years_active
+  @@all = []
 
-  def initialize(name, years_active)
-    @name = name
+  def initialize(artist_name, years_active)
+    @artist_name = artist_name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.map do |painting|
+      painting.artist == self
+      painting
+    end
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery}
+  end
+
+  def gallery_cities
+    galleries.map{|gallery| gallery.city}
+  end
+
+  def self.total_experience
+    self.all.map{|artist| artist.years_active}
+  end
+
+  def self.average_experience
+    self.total_experience.inject(:+) / self.all.count.to_f
+  end
 
 end
